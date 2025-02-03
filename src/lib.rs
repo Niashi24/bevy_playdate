ab5dbfb925c034cbad8d2a863d0ec9821496ab4d
Niashi <niasparacino4@gmail.com>
2025-02-03 21:43:09 +0100

    is anyone there

--- src/lib.rs
+++ src/lib.rs
@@ -17,6 +17,7 @@ pub struct DefaultPlugins;
 
 impl Plugin for DefaultPlugins {
     fn build(&self, app: &mut App) {
+        playdate::println!("test");
         app.add_plugins((
             input::InputPlugin,
             sprite::SpritePlugin,
